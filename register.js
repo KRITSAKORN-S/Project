77064b7907810bbfd23b315a1bf7f084b43f662d
KRITSAKORN-S <kritsakornsamakul61@gmail.com>
2022-04-14 20:35:10 +0700

    update

---
+++ register.js
@@ -0,0 +1,14 @@
+
+function show(){
+    var fullname = document.getElementById("name").value;
+    var birthday = document.getElementById("birthday").value;
+    var studentID = document.getElementById("stid").value;
+    var peopleID = document.getElementById("pid").value;
+    var phone = document.getElementById("phonenumber").value;
+
+    console.log(fullname);
+    console.log(birthday);
+    console.log(studentID);
+    console.log(peopleID);
+    console.log(phone);
+}
